7432f84a9572ec1bfdf81483a60f904caaab4236
Iimeware <119041536+Iimeware@users.noreply.github.com>
2025-04-09 13:35:16 -0700

    yay

---
+++ crypto-landing/app/layout.tsx
@@ -0,0 +1,30 @@
+import type React from "react"
+import "./globals.css"
+import type { Metadata } from "next"
+import { ThemeProvider } from "@/components/theme-provider"
+
+export const metadata: Metadata = {
+  title: "PUNCHMADE - $1 SCAM BIBLE",
+  description:
+    "punchmade done had enough.. giving hella sauce to the squad for low. 80+ untapped plays by punchmade dev.",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="font-mono">
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+
+import './globals.css'
